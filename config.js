d615c44291e54f7c537857844023e89e0ed2a6a9
mg6506144-cyber <mg6506144@gmail.com>
2026-05-03 01:17:46 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const BOT_TOKEN = "8529411138:AAHoxy_H8vWMS7A4uT188-6jehySe2uj_mI";
+
+export const ADMIN_ID = 8136997138;
+
+// Only Method channel is required for access
+export const METHOD_CHANNEL = "@Global_Method_Channel";
